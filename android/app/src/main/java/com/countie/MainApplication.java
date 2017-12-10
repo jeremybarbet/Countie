e9f9f3c1aeb22eaea181d7c99e28c237fce5912e
Jérémy Barbet <jeremgraph@gmail.com>
2017-12-10 21:45:02 +0000

    Android is back

--- android/app/src/main/java/com/countie/MainApplication.java
+++ android/app/src/main/java/com/countie/MainApplication.java
@@ -1,35 +1,34 @@
 package com.countie;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.reactnativenavigation.NavigationApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+      new ReactNativePushNotificationPackage(),
+      new LinearGradientPackage()
+    );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
 
   @Override
