944ec908b508b8417fb1534915eb1f93c52b9760
Jérémy Barbet <jeremgraph@gmail.com>
2017-12-25 17:53:46 +0000

    Android build steps

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,5 +2,8 @@
 cd ./node_modules/react-native-navigation
 patch -p1 < ../../scripts/patches/rcc-manager-module.patch
 
+cd ../react-native
+patch -p1 < ../../scripts/patches/react-gradle.patch
+
 cd ../react-native-swiper
 patch -p1 < ../../scripts/patches/pagination-shown.patch
